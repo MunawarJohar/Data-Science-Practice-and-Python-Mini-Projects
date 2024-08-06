8e0ae3a50040a837ab040158b78eb7cab6f24e1a
Munawar Johar <106137102+MunawarJohar@users.noreply.github.com>
2024-08-06 14:10:46 +0500

    push file

---
+++ L1_Data_science_intro.py
@@ -0,0 +1,17 @@
+# Python data science course         data analysics this course is free of cost  1.3 youtube views 1 lac tweets   
+print("Hello")
+
+# star=5
+# for i in range(star):
+#     print(" "*(star-i-1)+"*"*(i+1))
+    
+        
+        
+
+year=int(input("Enter a 4 digit year "))
+if year%4 ==0 and (year%100!=0)or year%400 == 0:
+    print("The year is leap year ")
+    
+else:
+    print("The year is not a leap year")
+        
